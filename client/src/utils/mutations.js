3ea18002b389432d73d91627c38ccc28c894b116
Vlah <brennan.vlahcevic@armssoftware.com>
2022-10-23 01:44:50 -0400

    minor login error

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,41 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($favorite: Favorites!) {
+    saveBook(favorite: $favorite) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
